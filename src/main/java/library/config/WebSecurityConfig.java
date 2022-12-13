494c1fcb631899dcc991c823f29199a46f2615b6
mikhailkatarzhin <maikl.1997@mail.ru>
2022-12-14 00:10:46 +0400

    [init] init Security configs

---
+++ src/main/java/library/config/WebSecurityConfig.java
@@ -0,0 +1,69 @@
+package library.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public static BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public WebSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/administration/**")
+                        .hasAnyAuthority("ADMINISTRATOR")
+                    .antMatchers("/reader/**")
+                        .hasAnyAuthority("READER")
+                    .antMatchers("/author/**")
+                        .hasAnyAuthority("AUTHOR")
+                    .antMatchers("/profile/**")
+                        .fullyAuthenticated()
+                    .antMatchers("/", "/webjars/**", "/css/**", "/home", "/sign_up")
+                        .permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/sign_in")
+                    .defaultSuccessUrl("/profile")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder())
+                .usersByUsernameQuery("select username, password, 'true' from user where username=?")
+                .authoritiesByUsernameQuery("\n" +
+                        "select u.nickname, r.name from user u" +
+                        " inner join user_role_set ur on u.id = ur.user_id" +
+                        " inner join role r on ur.role_set_id = r.id" +
+                        " where u.nickname=$");
+    }
+}
