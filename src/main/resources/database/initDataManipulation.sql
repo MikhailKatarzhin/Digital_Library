46181c549ddfa616b0fc3fafa01899b284ed6437
mikhailkatarzhin <maikl.1997@mail.ru>
2022-12-15 18:03:18 +0400

    [feat] add DataManipulation sql init

---
+++ src/main/resources/database/initDataManipulation.sql
@@ -0,0 +1,34 @@
+INSERT INTO
+    "User_status" (id, name)
+VALUES
+       (1, 'ACTIVE'), (2, 'BANNED'), (3, 'DELETED')
+ON CONFLICT (id)
+    DO UPDATE SET name = Excluded.name;
+
+INSERT INTO
+    "Role_of_User" (id, name)
+    VALUES
+       (1, 'ADMINISTRATOR'), (2, 'READER'), (3, 'AUTHOR')
+ON CONFLICT (id)
+    DO UPDATE SET id = Excluded.id;
+
+INSERT INTO
+    "User" (id, username, email, status_id, password)
+     VALUES
+        (0, 'administrator', 'maikl.1997@mail.ru', 1, '$2a$10$m/GwtXT2pD5VeGgpdGkNLeZVg9C0NG/sRhiPkkvKXrZj6wXtiUJze')
+ON CONFLICT (id)
+    DO NOTHING;
+
+INSERT INTO
+    "User_Role" (user_id, role_id)
+VALUES
+    (0, 1), (0, 2), (0, 3)
+ON CONFLICT (user_id, role_id)
+    DO NOTHING;
+
+INSERT INTO
+    "Wallet" (id, balance)
+VALUES
+    (0, 100000)
+ON CONFLICT (id)
+    DO NOTHING;
